d9d23f50d4c9a72fc822db1ca3a7625deb898f60
hurduring <idontwanttogoonthecart@gmail.com>
2016-02-16 15:05:54 +0300

    doing boilerplate

---
+++ source/app.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Hello extends Component {
+
+    render() {
+
+        return (
+
+            <h1>Heldfsafdsfsdaflo</h1>
+
+        );
+
+    }
+
+}
+
+React.render(<Hello />, document.getElementById('root'));
